92424851276b8719b2110f52c6c7409ebe6d237e
Mark Zaitsev <mark.zaytsev@yandex.ru>
2023-08-07 15:50:43 +0200

    added routes/movies and finished app

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
-// const cors = require('./middlewares/cors');
 const error = require('./middlewares/error');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -14,7 +13,6 @@ const app = express();
 app.use(express.json());
 
 app.use(requestLogger);
-// app.use(cors);
 app.use(helmet());
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', { family: 4, useNewUrlParser: true, useUnifiedTopology: true });
---
+++ routes/movies.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
+const {
+  getMovies,
+  createMovie,
+  deleteMovie,
+} = require('../controllers/movies');
+
+router.get('/', getMovies);
+
+router.post(
+  '/',
+  celebrate({
+    body: Joi.object().keys({
+      country: Joi.string().required(),
+      director: Joi.string().required(),
+      duration: Joi.string().integer().required(),
+      year: Joi.string().required(),
+      description: Joi.string().required(),
+      image: Joi.string().required().pattern(/(https)?:\/\/(www\.)?[a-z0-9\-._~:/?#[\]@!$&'()*+,;=]{2,}\.[a-z0-9/#?]{2,}/),
+      trailerLink: Joi.string().required().pattern(/(https)?:\/\/(www\.)?[a-z0-9\-._~:/?#[\]@!$&'()*+,;=]{2,}\.[a-z0-9/#?]{2,}/),
+      thumbnail: Joi.string().required().pattern(/(https)?:\/\/(www\.)?[a-z0-9\-._~:/?#[\]@!$&'()*+,;=]{2,}\.[a-z0-9/#?]{2,}/),
+      movieId: Joi.number().required(),
+      nameRU: Joi.string().required(),
+      nameEN: Joi.string().required(),
+    }),
+  }),
+  createMovie,
+);
+
+router.delete(
+  '/:_id',
+  celebrate({
+    params: Joi.object().keys({
+      _id: Joi.string().length(24).hex().required(),
+    }),
+  }),
+  deleteMovie,
+);
